0072a8e274431389a11ad249cd70dafbb7634213
Zahscha Gonzalez <zahsmar1489@fullsail.edu>
2013-02-13 14:57:19 -0500

    File Structure is added to the project

---
+++ php/index.php
@@ -0,0 +1,8 @@
+<?php
+	
+//Call database, model and view
+	require_once('db.php');
+	require_once('google-api-php-client/src/Google_Client.php');
+	require_once('google-api-php-client/src/contrib/Google_PlusService.php');
+	session_start();
+?>
