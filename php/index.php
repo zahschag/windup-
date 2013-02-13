03c7dc6ecfd45e6234fe5cc83f382db4dfd2a8c9
Zahscha Gonzalez <zahsmar1489@fullsail.edu>
2013-02-13 15:05:45 -0500

    Add API client

--- php/index.php
+++ php/index.php
@@ -2,7 +2,42 @@
 	
 //Call database, model and view
 	require_once('db.php');
+	require_once('tasksModel.php');
+	require_once('taskView.php'); 
 	require_once('google-api-php-client/src/Google_Client.php');
 	require_once('google-api-php-client/src/contrib/Google_PlusService.php');
 	session_start();
+	
+	$client = new Google_Client();
+$client->setApplicationName('Google+ PHP Starter Application');
+// Visit https://code.google.com/apis/console?api=plus to generate your
+// client id, client secret, and to register your redirect uri.
+$client->setClientId('320429132956.apps.googleusercontent.com');
+$client->setClientSecret('insert_your_oauth2_client_secret');
+$client->setRedirectUri('insert_your_oauth2_redirect_uri');
+$client->setDeveloperKey('AIzaSyBfT65XqPlzLvRfbr238JUn08i3LkHrRcA');
+$plus = new Google_PlusService($client);
+
+if (isset($_GET['code'])) {
+  $client->authenticate();
+  $_SESSION['token'] = $client->getAccessToken();
+  $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+  header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
+}
+
+if (isset($_SESSION['token'])) {
+  $client->setAccessToken($_SESSION['token']);
+}
+
+if ($client->getAccessToken()) {
+  $activities = $plus->activities->listActivities('me', 'public');
+  print 'Your Activities: <pre>' . print_r($activities, true) . '</pre>';
+
+  // We're not done yet. Remember to update the cached access token.
+  // Remember to replace $_SESSION with a real database or memcached.
+  $_SESSION['token'] = $client->getAccessToken();
+} else {
+  $authUrl = $client->createAuthUrl();
+  print "<a href='$authUrl'>Connect Me!</a>";
+}
 ?>
