f068131b0868215170573f79b4a4e1d291e76a85
Zahscha Gonzalez <zahsmar1489@fullsail.edu>
2013-02-13 22:06:16 -0500

    Base File Structure (MVC)

---
+++ controllers/db.php
@@ -0,0 +1,7 @@
+<?php
+	
+	const mydns = "mysql:host=127.0.0.1;port=8889;dbname=windup";
+	const myuser = "root";
+	const mypass = "root";
+	
+?>
---
+++ controllers/index.php
@@ -0,0 +1,7 @@
+<?php
+	
+//Call database, model and view
+	require_once('db.php');
+	require_once('tasksModel.php');
+	require_once('taskView.php'); 
+?>
