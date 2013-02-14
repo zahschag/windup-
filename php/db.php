05bd07747def44e4f298c579f34f1f75e120999c
Zahscha Gonzalez <zahsmar1489@fullsail.edu>
2013-02-13 22:12:40 -0500

    Delete

--- php/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-	const mydns = "mysql:host=127.0.0.1;port=8889;dbname=windup";
-	const myuser = "root";
-	const mypass = "root";
-	
-?>
--- php/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-//Call database, model and view
-	require_once('db.php');
-	//require_once('tasksModel.php');
-	//require_once('taskView.php'); 
-	echo "Hello WORLD!!"
-?>
