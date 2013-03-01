10df863353f9361ef669492b95774c3eb3cabca3
Zahscha Gonzalez <zahscha.gonzalez@gmail.com>
2013-03-01 08:30:56 -0500

    cooments were added to the php login file

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
-unset($_SESSIONS ['userInfo']);
-session_destroy($_SESSION['userInfo']);
-session_regenerate_id(true);
-header('Locations: auth.php');
-exit;
+session_start(); //will start the session
+unset($_SESSIONS ['userInfo']);//will unset the session
+session_destroy($_SESSION['userInfo']);//Will destroy the session 
+session_regenerate_id(true);//Will allow the user to log in again
+header('Locations: auth.php');//will take the users back to the auth.php page
+exit;//will exit the program
 ?>
