f42246a69c0b295004f382ec22baab33bc805274
Zahscha Gonzalez <zahscha.gonzalez@gmail.com>
2013-02-28 23:46:57 -0500

    CRUD Architecture set

--- models/tasksModel.php
+++ models/tasksModel.php
@@ -14,7 +14,7 @@ class TaskModel{
 				var_dump($l);
 			}//catch function
 		}//construct function
-		
+/*---------------------------------READ FUNCTIONS -------------------------------*/	
 	public function getTasksList(){
 		$statement = $this->db->prepare("
 			SELECT taskId, taskTitle, task, taskType, priority, date
@@ -46,26 +46,96 @@ class TaskModel{
 			}//conditional
 		}	//try
 		catch(\PDOException $l){
-			var_dump($l);
+			echo "Query Failed";
 			}
 			return array();
 		}
-		
-	//Function that will insert new users into the database
+/*-------------------------Task create function ------------------------------- */
+public function createNewTask($taskTitle, $task, $taskType, $priority){
+	$statement = $this->db->prepare("
+		INSERT INTO `tasks`(`taskTitle`, `task`, `taskType`, `priority`)
+		VALUES(:taskTitle, :task, :taskType, :priority)
+	");
+	try{
+		if($statement->execute(array(':taskTitle'=>$taskTitle, ':task'=>$task, ':taskType'=>$taskType,':priority'=>$priority))){
+			
+			$rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
+			return $rows;
+			
+		}//end of try
+	}
+	catch(\PDOException $l){
+			echo "Query Failed";
+		}
+}//end of create function
+/*-------------------------Task Update function ------------------------------- */
+public function updateTask($id, $task, $priority){
+$statement = $this->db->prepare("
+		UPDATE 'tasks' set
+			`task` = :task,
+			`priority` = :priority
+		WHERE `taskId` = :id
+	");
+	try{
+		if($statement->execute(array(':id'=>$id, ':task'=>$task, ':priority'=>$priority))){
+			$rows = $statement->fetchAll(\PDO:: FETCH_ASSOC);
+			return $rows;
+		}//end of try
+	}
+	catch(\PDOException $t){
+		echo "Query Failed";
+	}//end of catch
+}//end of update function
+
+/*-------------------------Task delete function ------------------------------- */
+public function deleteTask($id){
+	$statement = $this->db->prepare("
+		DELETE FROM `tasks`
+		WHERE 'taskId' = :id
+		");
+	try{
+		if($statement->execute(array(':id'=>$id,))){
+			$rows = $statement->fetchAll(\PDO:: FETCH_ASSOC);
+			return $rows;
+		}//end of try
+	}
+	catch(\PDOException $t){
+		echo "Query Failed";
+	}//end of catch
+}//end of Delete function
+/*---------------------------------------User functions ---------------------------------------------*/
+
+	/*-------------------Create New user function-------------------------*/
 	public function newUser(){
 		$statement = $this->db->prepare("
-		INSERT INTO users(user_fullname, lastname, user_name, user_password)
-		VALUES(user_fullname = :fullname, lastname = :lastname, user_name = :username, user_password = :password)
+			INSERT INTO `users`(`user_fullname`, `lastname`, `user_name`, `user_password`)
+			VALUES(:name, :lastname, :username, :password)
 		");
 		try{
-			if ($statement->execute(array(":fullname"=>$fullname, ":lastname"=>$lastname, ":username"=>$username,":password"=>$password))){
+			if ($statement->execute(array(':name'=>$name, ':lastname'=>$lastname, ':username'=>$username, ':password'=>$password))){
 				$rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
 				return $rows;
 			}//conditional
 		}//try
 		catch(\PDOException $l){
-			var_dum($l);
+			echo "Query Failed";
 		}//catch
+	}
+	public function updatePassword($id, $password){
+		$statement = $this->db->prepare("
+		UPDATE `users` set
+			`password` = :password,
+		WHERE `userId` = :id
+		");
+			try{
+				if($statement->execute(array(":id"=>$id, ":password"=>$password))){
+					$rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
+					return $rows;
+					}//conditional
+			}//try
+		catch(\PDOExeption $l){
+			var_dum($l);
+			}//catch
 	}
 }
 ?>
